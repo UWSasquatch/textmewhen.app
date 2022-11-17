3a876b486f77c3047e833cbea78bd387768b2246
Yash Arora <yasharora0606@gmail.com>
2022-11-16 19:45:45 -0500

    Not necessary, switching to next js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+
+
 function App() {
+
+  const items = ["you're home", "you're back", "you arrive", "you're safe"]
+  const [itemIndex, setItemIndex] = useState(0)
+
+  // const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setItemIndex((itemIndex + 1) % items.length)
+      console.log(itemIndex)
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+
+  }, [itemIndex]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="landing-page">
+      <header className="header">
+        TextMeWhen is currently in closed beta.
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sign up for the closed beta.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <input type="text" id="fname" name="fname"/>
+          <button>Submit</button>
+        </form>
       </header>
+
     </div>
   );
 }
